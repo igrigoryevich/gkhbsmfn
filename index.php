4b8590a9c5db438b232b40b55f7c070a4f1da4c7
Ihor Nedopas <i.grigoryevich@gmail.com>
2017-10-16 13:46:11 +0300

    Upgrade project

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
-require '../vendor/autoload.php';
-
-namespace AppBundle\Controller;
+require_once __DIR__.'/vendor/autoload.php';
+//namespace AppBundle\Controller;
 
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Finder\Finder;
 $finder = new Finder();
-$finder->in('../data/');
+$finder->in('src/');
 
 class LuckyController
 {
